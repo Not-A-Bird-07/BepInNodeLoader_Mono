cafcd06e7ed699488f03f1a5f27d028672e4af98
notabird <NotABird07@outlook.com>
2024-08-12 19:08:54 -0700

    added random number

---
+++ BepInNodeLoader/Nodes/Math/Random Number.cs
@@ -0,0 +1,37 @@
+﻿using BepInNodeLoader.Core;
+using BepInNodeLoader.CustomAttributes;
+using System;
+using System.Collections.Generic;
+
+namespace BepInNodeLoader.Nodes.Math
+{
+    public class Random_Number_Float : Node
+    {
+        public float First { get; set; }
+        public float Second { get; set; }
+
+        [IsArgOut]
+        public float Result { get; set; }
+
+        public override void Execute()
+        {
+            List<object> args = ArgumentsRetriever.GetArgumentsOf(this);
+            Result = UnityEngine.Random.Range((float)args[0], (float)args[1]);
+        }
+    }
+
+    public class Random_Number_Int : Node
+    {
+        public Int32 First { get; set; }
+        public Int32 Second { get; set; }
+
+        [IsArgOut]
+        public Int32 Result { get; set; }
+
+        public override void Execute()
+        {
+            List<object> args = ArgumentsRetriever.GetArgumentsOf(this);
+            Result = UnityEngine.Random.Range((Int32)args[0], (Int32)args[1]);
+        }
+    }
+}
