e1a9a7e8ce745abe1e12cdcb28f48ad91e64a6a5
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-08-13 22:51:10 -0400

    wawa

---
+++ BepInNodeLoader/Nodes/Room/IsModdedRoom.cs
@@ -0,0 +1,16 @@
+﻿using BepInNodeLoader.CustomAttributes;
+using Photon.Pun;
+using System.Linq;
+
+namespace BepInNodeLoader.Nodes.Room;
+
+public class IsModdedRoom : Node
+{
+    [IsArgOut]
+    public bool IsModded { get; set; }
+
+    public override void Execute()
+    {
+        IsModded = PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties.Values.Contains("MODDED");
+    }
+}
